64d929e1cf25efec44906892a902e5066839a8ae
Talwinder Singh <singh.talwinder@live.com>
2022-06-22 22:22:57 +0100

    created app . component template, Dynamic value in template,multiple component,adding styles, click events, added react dev tools to browser,outputting list, props, reusing component,function as props,useEffect hooks(basic) and dependecies,using jason server,fetching data with useEffect, conditional loading massage,hadling fetch error

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,46 @@
-const Home = () => {
+import { useState, useEffect } from 'react';
+import BlogList from './bloglist';
 
-    const handleclick = () => {
-        console.log('Hello');
+
+const Home = () => {
+const [blogs, setBlogs] = useState (null);
+const [isPending, setIsPending] = useState(true);
+const [error,setError] = useState(null);
+
+// const [name, setName] = useState('TS');
+
+// const handleDelete = (id) =>{
+//     const newBlogs = blogs.filter(blog => blog.id !== id);
+//     setBlogs(newBlogs);
+// }
+
+useEffect(() =>{
+   fetch('http://localhost:8000/blogs')
+   .then( res => {    
+    if(!res.ok){
+        throw Error('Could not fetch the data for that resource')
     }
+    return res.json();
+   })
+   .then(data =>{
+    setBlogs(data);
+    setIsPending(false);
+    setError(null);
+   })
+   .catch((err =>{
+    setIsPending(false);
+    setError(err.massage);
+   }))
+} ,[]);
+
     return ( 
         <div className="home">
-            <h2>homepage</h2>
-            <button onClick={handleclick}>Click me</button>
+            {error && <div>{ error }</div> }
+            {isPending && <div> Loading...</div> }
+         {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+         {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'TS')} title="TS blogs"/> */}
+        {/* <button onClick={() => setName('LS')}>Change name</button>
+        <p>{ name }</p> */}
         </div>
      );
 }
